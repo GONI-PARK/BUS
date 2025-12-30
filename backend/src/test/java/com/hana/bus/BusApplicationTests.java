405cd64eec9f95c9bbd2a159f7c6d73bb53421ab
박성곤 <nalsil139@gmail.com>
2025-12-30 21:39:26 +0900

    Initial commit

---
+++ backend/src/test/java/com/hana/bus/BusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
