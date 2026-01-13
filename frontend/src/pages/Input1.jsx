d497a8059a1c778e4ad699b40ea811a978e56724
박성곤 <nalsil139@gmail.com>
2026-01-13 22:59:24 +0900

    feat: selectable table and data passing structure, button optimization

--- frontend/src/pages/Input1.jsx
+++ frontend/src/pages/Input1.jsx
@@ -1,21 +1,21 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import InputContainer from '../components/InputContainer';
+import Input1Table from '../components/Input1Table';
 import { useNavigate } from 'react-router-dom';
 
 const Input1 = () => {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav('/Input2');
-  };
-
   return (
     <div>
       <Header />
       <h1>Welcome to the Input1 Page</h1>
+
       <main className="container mt-4">
-        <button onClick={onClickButton}>Input2 페이지로 이동</button>
+        <Input1Table />
       </main>
+
+      <InputContainer targetPath="/Input2" label="Input2" />
+
       <Footer />
     </div>
   );
--- frontend/src/pages/Input2.jsx
+++ frontend/src/pages/Input2.jsx
@@ -1,21 +1,14 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import InputContainer from '../components/InputContainer';
 import { useNavigate } from 'react-router-dom';
 
 const Input2 = () => {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav('/Input3');
-  };
-
   return (
     <div>
       <Header />
       <h1>Welcome to the Input2 Page</h1>
-      <main className="container mt-4">
-        <button onClick={onClickButton}>Input3 페이지로 이동</button>
-      </main>
+      <InputContainer targetPath="/Input3" label="Input3" />
       <Footer />
     </div>
   );
